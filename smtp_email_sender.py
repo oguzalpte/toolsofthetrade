d083e5b0b4811e01ce631554edc0abe1cfabc96d
oguzalpte <114469825+oguzalpte@users.noreply.github.com>
2022-09-27 10:54:37 +0300

    Update smtp_email_sender.py

--- smtp_email_sender.py
+++ smtp_email_sender.py
@@ -8,8 +8,8 @@ Created on Mon Nov  1 16:57:30 2021
 import smtplib, ssl
 from email.mime.text import MIMEText
 
-sender = 'ebys@marmaraosb.org'
-receivers = ['oguzhan.alptekin@cbksoft.com', 'alptekinoguz@gmail.com']
+sender = 'SENDER_MAIL'
+receivers = ['RECIEVER_MAIL']
 body_of_email = 'Test Email'
 
 msg = MIMEText(body_of_email, 'html')
@@ -19,18 +19,13 @@ msg['To'] = ",".join(receivers)
 
 #possible ports: 465, 587
 
-s = smtplib.SMTP(host = 'mail.marmaraosb.org', port = 587)
-s.login(user = 'ebys@marmaraosb.org', password = 'AWSgMRfy3J(J')
+#USE smtplib.SMTP_SSL if SSL
+
+s = smtplib.SMTP(host = 'SMTP', port = 587)
+s.login(user = 'USER', password = 'PASS')
 s.sendmail(sender, receivers, msg.as_string())
 print('email sent')
 s.quit()
 
 
 
-"""SSL
-s = smtplib.SMTP_SSL(host = 'mail.marmaraosb.org', port = 587)
-s.login(user = 'ebys@marmaraosb.org', password = 'AWSgMRfy3J(J')
-s.sendmail(sender, receivers, msg.as_string())
-print('email sent')
-s.quit()
-"""
